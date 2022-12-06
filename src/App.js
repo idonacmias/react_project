f36c791fbebdbfb20a50b7c2043eb887f52d7b70
idonacmias <idonacmi@gmail.com>
2022-12-06 17:18:01 +0200

    not shure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
+
+
+const App = () => {
+  const myStyle = {backgroundColor:"red", color: "green"}
+  const [counter, setCnt] = useState(0);
+  const listName = ['ido', 'bob', 'clum', 'mery', 'lisa'];
+  const dictName = [{'name':'ido', 'age':1}, {'name':'bob', 'age':2}]
+  const [userName, setUserName] = useState("enter your name")
+  const test = () => {
+      setCnt(counter+1);
+    };
+
+ const newFnc = () =>{
+    if (counter > listName.length-1){
+      setCnt(counter-counter);
+    
+    }else{
+      test()
+    }
+
+ };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick = {() => newFnc()}>{listName[counter]} {counter} </button>    
+        <input onChange={(e) => setUserName(e.target.value)}></input> 
+        <h1 style={myStyle}>{userName} </h1>  
+        {listName.map((name,i) => (
+        <div key={i}>{name}</div>
+        ))}
+
+        {dictName.map((stu,i)=>(
+          <div key={i}> {stu.name} : {stu.age}</div>
+          ))}
+        <inerComp>a</inerComp>
     </div>
-  );
+    );
 }
 
 export default App;
